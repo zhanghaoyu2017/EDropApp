bd4a855d8bd6112da3e779ade375d93d22ad6e69
chasewind233 <1131158275@qq.com>
2019-11-25 16:59:17 +0800

    创建垃圾分类说明第2、3、4个页面

--- app/src/main/java/net/edrop/edrop_user/activity/MainActivity.java
+++ app/src/main/java/net/edrop/edrop_user/activity/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-
-
 import net.edrop.edrop_user.R;
 import net.edrop.edrop_user.adapter.MyPagerAdapter;
 
---
+++ app/src/main/java/net/edrop/edrop_user/activity/RubbishDesc04Activity.java
@@ -0,0 +1,14 @@
+package net.edrop.edrop_user.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import net.edrop.edrop_user.R;
+
+public class RubbishDesc04Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rubbish_desc04);
+    }
+}
