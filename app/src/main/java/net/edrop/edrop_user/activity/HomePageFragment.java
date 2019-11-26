acfa1fc3adacbf53fcb504dcb8d21561b9045543
kangxiaohui <739243123@qq.com>
2019-11-26 08:35:16 +0800

    主页增加搜索框

--- app/src/main/java/net/edrop/edrop_user/activity/HomePageFragment.java
+++ app/src/main/java/net/edrop/edrop_user/activity/HomePageFragment.java
@@ -1,20 +1,103 @@
 package net.edrop.edrop_user.activity;
 
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import net.edrop.edrop_user.R;
 
-public class HomePageFragment extends Fragment {
+import java.lang.reflect.Field;
+
+public class HomePageFragment extends Fragment implements View.OnClickListener{
+    private View view;
+    private SearchView mSearchView;
+    private AutoCompleteTextView mAutoCompleteTextView;//搜索输入框
+    private ImageView mDeleteButton;//搜索框中的删除按钮
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View newView = inflater.inflate(R.layout.fragment_home_page, container, false);
-        return newView;
+        view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        initView();
+        initData();
+        setListener();
+        return view;
+    }
+    private void initView(){
+        mSearchView=view.findViewById(R.id.view_search);
+        mAutoCompleteTextView=mSearchView.findViewById(R.id.search_src_text);
+        mDeleteButton=mSearchView.findViewById(R.id.search_close_btn);
+    }
+
+    private void initData(){
+        mSearchView.setIconifiedByDefault(false);//设置搜索图标是否显示在搜索框内
+        //1:回车
+        //2:前往
+        //3:搜索
+        //4:发送
+        //5:下一項
+        //6:完成
+        mSearchView.setImeOptions(2);//设置输入法搜索选项字段，默认是搜索，可以是：下一页、发送、完成等
+//        mSearchView.setInputType(1);//设置输入类型
+//        mSearchView.setMaxWidth(200);//设置最大宽度
+        mSearchView.setQueryHint("搜索");//设置查询提示字符串
+//        mSearchView.setSubmitButtonEnabled(true);//设置是否显示搜索框展开时的提交按钮
+        //设置SearchView下划线透明
+        setUnderLinetransparent(mSearchView);
+    }
+
+    private void setListener(){
+        // 设置搜索文本监听
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //当点击搜索按钮时触发该方法
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.e("=query=",query+"ss");
+                return false;
+            }
+
+            //当搜索内容改变时触发该方法
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.e("=====newText=",newText);
+                return false;
+            }
+        });
+    }
+
+    /**设置SearchView下划线透明**/
+    private void setUnderLinetransparent(SearchView searchView){
+        try {
+            Class<?> argClass = searchView.getClass();
+            // mSearchPlate是SearchView父布局的名字
+            Field ownField = argClass.getDeclaredField("mSearchPlate");
+            ownField.setAccessible(true);
+            View mView = (View) ownField.get(searchView);
+            mView.setBackgroundColor(Color.TRANSPARENT);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+//            case R.id.btn_next:
+//
+//                break;
+//            default:
+//                break;
+        }
     }
 }
