13fa537e229e5d0b94b006e271fb1d6e58441e70
zhanghaoyu2017 <273903932@qq.com>
2019-11-26 10:52:00 +0800

    增加验证码登录界面

---
+++ app/src/main/java/net/edrop/edrop_user/activity/PhoneLoginActivity.java
@@ -0,0 +1,16 @@
+package net.edrop.edrop_user.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import net.edrop.edrop_user.R;
+
+public class PhoneLoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_login);
+    }
+
+}
