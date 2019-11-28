96edb8963e66c2ac6dadc5cbf5449835419c4f50
kangxiaohui <739243123@qq.com>
2019-11-28 09:18:51 +0800

    增加消息信息栏

--- app/src/main/java/net/edrop/edrop_user/activity/MsgPageFragment.java
+++ app/src/main/java/net/edrop/edrop_user/activity/MsgPageFragment.java
@@ -7,8 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import net.edrop.edrop_user.R;
+import net.edrop.edrop_user.adapter.MsgSwipeAdapter;
+import net.edrop.edrop_user.entity.MsgItemBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mysterious
@@ -17,6 +23,10 @@ import net.edrop.edrop_user.R;
  * Time: 16:40
  */
 public class MsgPageFragment extends Fragment {
+    private ListView listView;
+    private List<MsgItemBean> datas = new ArrayList<>();
+    private MsgSwipeAdapter swipeAdapter;
+    private View view;
     private static final String SECTION_STRING = "fragment_string";
 
     public static MsgPageFragment newInstance(String sectionNumber) {
@@ -26,10 +36,28 @@ public class MsgPageFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View newView = inflater.inflate(R.layout.fragment_msg_page, container, false);
-        return newView;
+        view = inflater.inflate(R.layout.fragment_msg_page, container, false);
+        initData();
+        initView();
+        return view;
+    }
+
+    private void initData() {
+        for (int i = 0; i < 15; i++) {
+            MsgItemBean itemBean = new MsgItemBean();
+            itemBean.setNickName("昵称 " + (i + 1));
+            itemBean.setMsg("Message " + i);
+            datas.add(itemBean);
+        }
+    }
+
+    private void initView() {
+        listView = view.findViewById(R.id.lv_main);
+        swipeAdapter = new MsgSwipeAdapter(getContext(), datas);
+        listView.setAdapter(swipeAdapter);
     }
 }
