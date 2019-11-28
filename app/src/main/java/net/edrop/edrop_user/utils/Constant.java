4bcf9831f3bb61f2580dc9ef94611df4ae121502
zhanghaoyu2017 <273903932@qq.com>
2019-11-28 17:40:43 +0800

    将拍照的图片上传至服务器（初步）

---
+++ app/src/main/java/net/edrop/edrop_user/utils/Constant.java
@@ -0,0 +1,11 @@
+package net.edrop.edrop_user.utils;
+
+/**
+ * Created by Android Studio.
+ * User: zhanghaoyu
+ * Date: 2019/11/28
+ * Time: 17:18
+ */
+public class Constant {
+    public static final String BASE_URL="http://10.7.88.157:8080/EDropService/";
+}
